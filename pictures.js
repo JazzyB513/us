b4b3d2f0d20ef893ab9f326890c0920f9cf4316f
JazzyB513 <crazytowndancer@gmail.com>
2025-09-05 20:18:30 -0600

    Update pictures.js

--- pictures.js
+++ pictures.js
@@ -1,42 +1,37 @@
 // pictures.js
+document.addEventListener("DOMContentLoaded", () => {
+  const imageUrls = [
+    "images/IMG_0802.JPG",
+    "images/IMG_0994.JPG",
+    "images/IMG_1148.JPG",
+    "images/IMG_1149.JPG",
+    "images/IMG_1344.JPG",
+    "images/IMG_1606.JPG",
+    "images/IMG_1632.JPG"
+  ];
 
-// List of image URLs
-const imageUrls = [
-  "images/IMG_0802.JPG",
-  "images/IMG_0994.JPG",
-  "images/IMG_1148.JPG",
-  "images/IMG_1149.JPG",
-  "images/IMG_1344.JPG",
-  "images/IMG_1606.JPG",
-  "images/IMG_1632.JPG"
-];
+  let currentIndex = 0;
+  const imgElement = document.getElementById("carousel-image");
 
-let currentIndex = 0;
-const imgElement = document.getElementById('carousel-image');
+  function showImage(index) {
+    imgElement.style.opacity = 0;
+    setTimeout(() => {
+      imgElement.src = imageUrls[index];
+      imgElement.onload = () => {
+        imgElement.style.opacity = 1;
+      };
+    }, 200);
+  }
 
-// Function to show image with fade effect
-function showImage(index) {
-  imgElement.style.opacity = 0; // fade out
+  window.nextImage = function() {
+    currentIndex = (currentIndex + 1) % imageUrls.length;
+    showImage(currentIndex);
+  };
 
-  setTimeout(() => {
-    imgElement.src = imageUrls[index];
-    imgElement.onload = () => {
-      imgElement.style.opacity = 1; // fade in
-    };
-  }, 200); // short delay for fade out
-}
+  window.prevImage = function() {
+    currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+    showImage(currentIndex);
+  };
 
-// Show next image
-function nextImage() {
-  currentIndex = (currentIndex + 1) % imageUrls.length;
   showImage(currentIndex);
-}
-
-// Show previous image
-function prevImage() {
-  currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
-  showImage(currentIndex);
-}
-
-// Initialize carousel
-showImage(currentIndex);
+});
